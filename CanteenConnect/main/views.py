a6daaf0f27ffaee4a519776c547701dffa140a75
Anant <90376578+Antverse1@users.noreply.github.com>
2023-02-21 17:08:02 +0530

    login system working

--- CanteenConnect/main/views.py
+++ CanteenConnect/main/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def login(request):
-    return render(request,"Login.html",{})
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'],password = request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render (request,'login.html', {'error':'Username or password is incorrect!'})
+    else:
+        return render(request,'login.html')
+
+@login_required
+def home(request):
+    return render(request,'home.html',{})
+
