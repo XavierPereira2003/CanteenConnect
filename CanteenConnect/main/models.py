b8b66622fec0b01ee4578f035a75a5fbaf10c950
DeonXavierPereira <deonxavier2019@gmail.com>
2023-03-04 12:10:21 +0530

    Added the Menu model

--- CanteenConnect/main/models.py
+++ CanteenConnect/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Menu(models.Model):
+    """
+    This Model is for to register user if they are coming to canteen
+    to eat.
+    """
+    Date=models.DateField(unique=True,primary_key=True)
+    Course=models.JSONField(default=list,blank=True)
+    Vistors_list=models.ManyToManyField(User,blank=True)
+
+    def __str__(self):
+        return str(self.Date)
+    
+
+    
