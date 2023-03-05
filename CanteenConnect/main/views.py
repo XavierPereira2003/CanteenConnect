00588e59bb89572bc961f6626c341a31772eb598
Anant <90376578+Antverse1@users.noreply.github.com>
2023-03-05 21:46:38 +0530

    Minor Changes

--- CanteenConnect/main/views.py
+++ CanteenConnect/main/views.py
@@ -3,16 +3,21 @@ from django.contrib.auth import authenticate
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import user_passes_test
 from .models import Menu
-# Create your views here.
 
-'''Tests Region'''
+
+#Testing
 def chef_check(user):
+    '''To check if user is chef'''
     return user.groups.filter(name='chef').exists()
 def student_check(user):
+    '''To check if user is student'''
     return user.groups.filter(name='student').exists()
-'''Tests Region'''
 
-def login(request): #allows user to log into site is capable of diffrentiating between chef and student
+
+
+#Functions 
+def login(request):
+    '''allows user to log into site is & capable of diffrentiating between chef and student'''
     if request.method == 'POST':
         user = authenticate(username=request.POST['username'],password = request.POST['password'])
         if user is not None:
@@ -30,6 +35,9 @@ def logout(request):
     auth.logout(request)
     return redirect('login')
 
+
+
+#Site Rendering Functions
 @user_passes_test(student_check)#to only allow if user is student
 def student_home(request):
     return render(request,'student_home.html',{})
